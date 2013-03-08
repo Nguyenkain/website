b755dd12a248242b3a792264d1d082c476b12d2f
LanNQ <Nguyenkain@gmail.com>
2013-03-08 12:21:27 +0700

    commit file up ảnh

--- protected/views/news/_form.php
+++ protected/views/news/_form.php
@@ -13,15 +13,25 @@
 
 <?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
 
-<?php $this->widget('bootstrap.widgets.TbFileUpload', array(
-		'url' => $this->createUrl("news/upload"),
-		'model' => $model,
-		'attribute' => 'picture', // see the attribute?
-		'multiple' => true,
-		'options' => array(
-    'maxFileSize' => 2000000,
-    'acceptFileTypes' => 'js:/(\.|\/)(gif|jpe?g|png)$/i',
-))); ?>
+<?php $this->widget('ext.EAjaxUpload.EAjaxUpload',
+		array(
+				'id'=>'uploadFile',
+				'config'=>array(
+						'action'=>Yii::app()->createUrl('news/upload'),
+						'allowedExtensions'=>array("jpg"),//array("jpg","jpeg","gif","exe","mov" and etc...
+						'sizeLimit'=>10*1024*1024,// maximum file size in bytes
+						'minSizeLimit'=>0.2*1024*1024,// minimum file size in bytes
+						'onComplete'=>"js:function(id, fileName, responseJSON){ alert(fileName); }",
+						//'messages'=>array(
+						//                  'typeError'=>"{file} has invalid extension. Only {extensions} are allowed.",
+						//                  'sizeError'=>"{file} is too large, maximum file size is {sizeLimit}.",
+						//                  'minSizeError'=>"{file} is too small, minimum file size is {minSizeLimit}.",
+						//                  'emptyError'=>"{file} is empty, please select files again without it.",
+						//                  'onLeave'=>"The files are being uploaded, if you leave now the upload will be cancelled."
+						//                 ),
+						//'showMessage'=>"js:function(message){ alert(message); }"
+				)
+)); ?>
 
 <?php echo $form->textAreaRow($model,'short_description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
