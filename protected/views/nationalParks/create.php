e090694f6e55361fb1d3fe969c7d525a83851a73
luuvn <asdf@asd.adf>
2013-03-13 16:45:24 +0700

    commit

---
+++ protected/views/nationalParks/create.php
@@ -0,0 +1,12 @@
+<?php $this->breadcrumbs = array(
+		'Vườn Quốc Gia' => array('index'),
+		'Tạo mới',
+		);
+
+	$this->menu = array(
+		array('label' => 'Liệt kê Vườn Quốc Gia', 'url' => array('index')),
+		array('label' => 'Quản lý Vườn Quốc Gia', 'url' => array('admin')),
+		); ?>
+
+<h1>Tạo mới Vườn Quốc Gia</h1>
+<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
---
+++ protected/views/nationalParks/update.php
@@ -0,0 +1,18 @@
+<?php
+$this->breadcrumbs=array(
+	'Vườn Quốc Gia'=>array('index'),
+	$model->id=>array('view','id'=>$model->id),
+	'Cập nhật',
+);
+
+$this->menu=array(
+	array('label'=>'Liệt kê Vườn Quốc Gia','url'=>array('index')),
+	array('label'=>'Tạo mới Vườn Quốc Gia','url'=>array('create')),
+	array('label'=>'Xem Vườn Quốc Gia','url'=>array('view','id'=>$model->id)),
+	array('label'=>'Quản lý Vườn Quốc Gia','url'=>array('admin')),
+);
+?>
+
+<h1>Cập nhật Vườn Quốc Gia <?php echo $model->id; ?></h1>
+
+<?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
