27c28b75624b8eef4be702b6da020288ebcc738c
chinhnt <picachino1991@yahoo.com>
2013-03-14 12:25:36 +0700

    Commit Chinh

--- protected/views/creatures/_form.php
+++ protected/views/creatures/_form.php
@@ -38,7 +38,7 @@
 	<?php $this->widget('application.extensions.tinymce.ETinyMce',
 			array('model'=>$model,
                        'attribute'=>'Description',
-                       'editorTemplate'=>'simple',
+                       'editorTemplate'=>'full',
                         'htmlOptions'=>array('rows'=>6, 'cols'=>50, 'class'=>'tinymce'),)); ?>
 	<?php echo $form->error($model,'Description'); ?>
 </div>
