ceca329bec9feaab843061da88125f9f35261c94
luuvn <asdf@asd.adf>
2013-03-18 15:08:57 +0700

    create model
    Signed-off-by: luuvn <asdf@asd.adf>

---
+++ protected/models/Coordinations.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('application.models._base.BaseCoordinations');
+
+class Coordinations extends BaseCoordinations
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
