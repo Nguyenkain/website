466974a254b63d53b9bf26ccbc31ae829d0361ed
LanNQ <Nguyenkain@gmail.com>
2013-04-03 10:31:46 +0700

    Sửa lại file main

--- frontend/views/layouts/layout_normal.php
+++ frontend/views/layouts/layout_normal.php
@@ -1,12 +1,5 @@
 <?php /* @var $this Controller */ ?>
 
-<script type="text/javascript">
-$(document).ready(function() {
-	$('#footer_content').hide();
-	$('#footer').addClass('normal');
-});
-
-</script>
 
 <?php $this->beginContent('//layouts/main'); ?>
 		<div id="page">
